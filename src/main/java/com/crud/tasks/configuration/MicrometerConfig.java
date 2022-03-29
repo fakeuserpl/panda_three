8ea50f759908252c4a3a0ddfa2d847a777fdd61b
Monika Zamyslowska <zamyslowska.monika@gmail.com>
2022-03-29 10:04:47 +0200

    Added Prometheus monitoring

---
+++ src/main/java/com/crud/tasks/configuration/MicrometerConfig.java
@@ -0,0 +1,18 @@
+package com.crud.tasks.configuration;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.spring.autoconfigure.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MicrometerConfig {
+
+    @Bean
+    MeterRegistryCustomizer meterRegistryCustomizer(MeterRegistry meterRegistry) {
+        return meterRegistry1 -> {
+            meterRegistry.config()
+                    .commonTags("application", "micrometer-example");
+        };
+    }
+}
